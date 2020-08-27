160867e9a9a4714fcbb75ad60b7143b00bc5a7ff
amankkg <amankubanychbek@gmail.com>
2020-08-27 19:52:16 +0600

    add AsyncFileIO example and pring directories and files

---
+++ AsyncFileIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace AsyncFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var relativePath = ".";
+
+            Console.WriteLine($"Working directory: \"{Path.GetFullPath(relativePath)}\"");
+
+            Console.WriteLine("\nDirectories:");
+            foreach (var directory in Directory.GetDirectories(relativePath))
+                Console.WriteLine($"\t{directory}");
+
+            Console.WriteLine("Files:");
+            foreach (var filename in Directory.GetFiles(relativePath))
+                Console.WriteLine($"\t{filename}");
+        }
+    }
+}
